ea02468bcf2858ceecc9a517083df25e5fbf8da6
kaushaltheeG <kaushalsk09@gmail.com>
2023-03-07 19:40:36 -0800

    hehe 6:

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,23 +5,21 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             term: ''
         }
-        
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
     }
 
     search() {
         this.props.onSearch(this.state.term);
-        //alert('working');
+        //alert('working');s
     }
 
-    handleTermChange(event) {
-        this.setState({term: event.target.value});
-    }
 //
     render() {
         return (
